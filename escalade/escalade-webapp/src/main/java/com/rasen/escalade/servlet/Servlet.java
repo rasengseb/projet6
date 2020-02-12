3e82d2ce0b784c6e5cadaf5a5a3d8e4fb9b6835f
Sébastien LEBAIL <deathseb@gmail.com>
2020-02-12 20:41:29 +0100

    Ajout fiches descriptive + début page web

---
+++ escalade/escalade-webapp/src/main/java/com/rasen/escalade/servlet/Servlet.java
@@ -0,0 +1,21 @@
+package com.rasen.escalade.servlet;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class Servlet extends HttpServlet {
+
+    public Servlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletReponse reponse) throws ServletExecption, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletReponse reponse) throws ServletExecption, IOException {
+
+    }
+}
