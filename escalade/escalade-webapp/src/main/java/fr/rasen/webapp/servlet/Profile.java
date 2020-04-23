a78d0430eceac406c0f1ab60294b2de86180fae4
rasengseb <deathseb@gmail.com>
2020-04-23 09:18:44 +0200

    Correction problème lancement

--- escalade/escalade-webapp/src/main/java/fr/rasen/webapp/servlet/Profile.java
+++ escalade/escalade-webapp/src/main/java/fr/rasen/webapp/servlet/Profile.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/profile")
+@WebServlet("/Profile")
 public class Profile extends HttpServlet {
 
     @Inject
